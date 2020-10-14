7a9ee01697b45f39548cc8d89e5f78c90c04368b
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-14 19:38:26 +0200

    MySecondCSApp : 04
    Proyect form number 4 completed.

---
+++ MySecondCSApp_04/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySecondCSApp_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum;
+            textBox3.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int res;
+            textBox3.Text = "-";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int calculate;
+            
+               
+            if (textBox3.Text == "+")
+            {
+                calculate = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                textBox4.Text = calculate.ToString();
+            } else
+            {
+                if(textBox3.Text == "-")
+                {
+                    calculate = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+                    textBox4.Text = calculate.ToString();
+                } else
+                {
+                    textBox4.Text = "error";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    }
+}
