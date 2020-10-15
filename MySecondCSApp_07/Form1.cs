d65ca37eea7a43f2aa5544ffde16c8c62ebde954
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-15 18:52:05 +0200

    MySecondCSApp : 07
    In this activity i created one comboBox and textBox.
    The idea are print little message ("the Choosen colour is: ") and
    acontinuation show the name from the colour selected.
    
    Unlike the last project forms i optimitzed the code with this function:
    
    		String res = "The choosen color is: ";
            textBox1.Text = res + comboBox1.SelectedItem.ToString();
    
    Also in this code for more optimization i create the objects string from comboBox
    at the start the app in to the 'public Form1()':
    
    		InitializeComponent();
            comboBox1.Items.Add("red");
            comboBox1.Items.Add("purple");
            comboBox1.Items.Add("blue");
            comboBox1.Items.Add("yellow");
            comboBox1.Items.Add("cyan");

---
+++ MySecondCSApp_07/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySecondCSApp_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("red");
+            comboBox1.Items.Add("purple");
+            comboBox1.Items.Add("blue");
+            comboBox1.Items.Add("yellow");
+            comboBox1.Items.Add("cyan");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                textBox1.Text = string.Empty;
+            }
+            else
+            {
+                String res = "The choosen color is: ";
+                textBox1.Text = res + comboBox1.SelectedItem.ToString();
+            }
+        }
+
+        /* 
+         * private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+              if (comboBox1.SelectedIndex == -1) {
+                textBox1.Text = string.Empty;
+              } else {
+                textBox1.Text = comboBox1.SelectedItem.ToString();
+              }
+            } 
+         */
+    }
+}
